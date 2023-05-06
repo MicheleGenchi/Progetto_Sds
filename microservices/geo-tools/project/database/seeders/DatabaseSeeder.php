6f3b7337b82c56d0e81e446b600723b6ad671544
Michele Genchi <cell.mgenchi@gmail.com>
2023-05-06 14:16:41 +0200

    manca use        Database\Seeders\CitiesSeeder;        Database\Seeders\Countrie;

--- microservices/geo-tools/project/database/seeders/DatabaseSeeder.php
+++ microservices/geo-tools/project/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\CountriesSeeder;
+use Database\Seeders\CitiesSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder

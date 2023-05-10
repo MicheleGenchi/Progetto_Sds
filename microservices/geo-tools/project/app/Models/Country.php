cdd013212163f216b92c6d77af680927030bc841
Michele Genchi <cell.mgenchi@gmail.com>
2023-05-10 11:08:32 +0200

    use try catch prima di $query

--- microservices/geo-tools/project/app/Models/Country.php
+++ microservices/geo-tools/project/app/Models/Country.php
@@ -11,6 +11,7 @@ use App\Traits\HttpCodeResponseTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\QueryException;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
@@ -94,9 +95,6 @@ class Country extends Model
      */
     public function get(array $filters): array
     {
-        ini_set('memory_limit', '-1');
-        ini_set('max_execution_time', 0); //300 seconds = 5 minutes
-        include_once 'HttpCodeResponse.php';
 
         # converte il campo ordine in maiuscolo "asc"="ASC", "desc"="DESC"
         # self::initFieldsUpperCase($filters, $fieldUpper=['ordine']);
@@ -106,8 +104,8 @@ class Country extends Model
             // the keys correspond to the keys in the input array
             'country_code' => new Assert\Optional(new Assert\All(self::getRules('country_code'))),
             'country' => new Assert\Optional(new Assert\All(self::getRules('country'))),
-            'ordine' => new Assert\Optional(self::getRules('ordine')),
-            'resultPerPage' => new Assert\Optional(self::getRules('resultPerPage'))
+            'resultPerPage' => new Assert\Optional(self::getRules('resultPerPage')),
+            'ordine' => new Assert\Optional(self::getRules('ordine'))
         ]);
 
         # WithValidationTrait
@@ -115,33 +113,41 @@ class Country extends Model
 
         if (count($errors)) {
             return [
-                'code' => HTTP_BAD_REQUEST,
-                'response' => ["error" => $errors]
+                'code' => self::HTTP_BAD_REQUEST,
+                'response' => ["errors" => $errors]
             ];
         }
 
         # join con la tabella geo
         # $query = self::join('geo', 'geo.nazione_code', '=', "{$this->table}.nazione_code")->select('*');
         # chiedere a Cosimo se è necessaria la join
+        try 
+        {
+            $query=self::select('*');
+            # filtra i dati
+            $query = (isset($filters["country_code"])) ? 
+                        $query->whereIn("{$this->table}.country_code", $filters["country_code"]) : $query;
+            $query = (isset($filters["country"])) ? 
+                        $query->whereIn("{$this->table}.country", $filters["country"]) : $query;
 
-        $query=self::select('*');
-        # filtra i dati
-        $query = (isset($filters["country_code"])) ? 
-                    $query->whereIn("{$this->table}.country_code", $filters["country_code"]) : $query;
-        $query = (isset($filters["country"])) ? 
-                    $query->whereIn("{$this->table}.country", $filters["country"]) : $query;
+            # ordina
+            $query = isset($filters['ordine']) ? self::ordina($query, $filters['ordine']) : $query;
 
-        # ordina
-        $query = isset($filters['ordine']) ? self::ordina($query, $filters['ordine']) : $query;
-
-        # DBUTilitities::paginate 
-        # se $resultPerPage>LIMITE_RISULTATI_PAGINA prende il limite
-        $rows=$query->paginate(self::paginate($filters["resultPerPage"] ?? self::LIMITE_RISULTATI_PAGINA));
- 
-        return [
-            'code' => HTTP_OK,
-            'response' => ['data' => $rows]
-        ];
+            # DBUTilitities::paginate 
+            # se $resultPerPage>LIMITE_RISULTATI_PAGINA prende il limite
+            $rows=$query->paginate(self::paginate($filters["resultPerPage"] ?? self::LIMITE_RISULTATI_PAGINA));
+    
+            return [
+                'code' => self::HTTP_OK,
+                'response' => ['data' => $rows]
+            ];
+        } catch (QueryException | Exception $e)  {
+          rollback();
+          return [
+                "code" => self::HTTP_INTERNAL_SERVER_ERROR,
+                "response" => ["message" => ERRORE_DATABASE]
+            ];  
+        }
     }
 
 }
